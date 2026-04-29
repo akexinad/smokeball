412563e8aee0c276658c0413894b34b931b87105
akexinad <danixeka@gmail.com>
2026-04-29 12:19:14 +1000

    feat: implemetation getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,33 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
+
+const statusKeyMap = {
+    200: "Arnie Quote",
+    500: "FAILURE",
+};
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+    // TODO: Implement this function.
+    if (!Array.isArray(urls)) {
+        return {
+            status: 400,
+            title: "Bad Request",
+            message: "URLs must be of type array",
+        };
+    }
+
+    const response = await Promise.all(urls.map((url) => httpGet(url)));
+
+    const results = response.map((res) => {
+        const body = JSON.parse(res.body);
+
+        return {
+            [statusKeyMap[res.status]]: body.message,
+        };
+    });
+
+    return results;
 };
 
 module.exports = {
-  getArnieQuotes,
+    getArnieQuotes,
 };
